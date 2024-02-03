5bab49e997180425e27d774acf9200e80541ee85
Gauresh G Pai <paigauresh@gmail.com>
2024-02-04 03:23:13 +0530

    upload

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
-const page = () => {
+const Home = () => {
   return (
-    <div>page</div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p>
+        Promptopia is an open-source AI prompting
+        tool for the modern world to
+        discover, create, and share creative prompts.
+      </p>
+    </section>
+  );
+};
 
-export default page
+export default Home;
