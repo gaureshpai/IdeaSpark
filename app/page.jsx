528e1b37fd9dd6161d22fdf94e3c164197ce83ff
Gauresh G Pai <paigauresh@gmail.com>
2024-02-04 03:49:27 +0530

    code upload

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,22 @@
 // pages/page.jsx
 import React from 'react';
-import Layout from '@components/Layout';
 
 const Home = () => {
   return (
-    <Layout>
       <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
           Discover & Share
           <br className="max-md:hidden" />
-          <span className="orange_gradient text-center">AI-Powered Prompts</span>
+          <span className="orange_gradient
+           text-center">AI-Powered Prompts</span>
         </h1>
-        <p>
+        <p className='desc text-center'>
           Promptopia is an open-source AI prompting
           tool for the modern world to
           discover, create, and share creative prompts.
         </p>
+        {/* Feed */}
       </section>
-    </Layout>
   );
 };
 
