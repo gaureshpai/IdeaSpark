5d68ba3af65d6921ae52e313eb7259761e744231
Gauresh G Pai <paigauresh@gmail.com>
2024-02-04 06:42:01 +0530

    route code written succesfully mongoose connected sucesfully user.js updated

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,19 +1,47 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+import User from '@models/User';
+import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: '',
-            clientSecret: '',
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
-    async session({ session }){
+    callbacks: {
+        async session({ session }) {
+            // store the user id from MongoDB to session
+            const sessionUser = await User.findOne({ email: session.user.email });
+            session.user.id = sessionUser._id.toString();
 
-    },
-    async signIn({profile}){
+            return session;
+        },
+        async signIn({ account, profile, user, credentials }) {
+            try {
+                await connectToDB();
 
+                // check if user already exists
+                const userExists = await User.findOne({ email: profile.email });
+
+                // if not, create a new document and save user in MongoDB
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture,
+                    });
+                }
+
+                return true
+            } catch (error) {
+                console.log("Error checking if user exists: ", error.message);
+                return false
+            }
+        },
     }
 })
 
-export {handler as GET , handler as POST};
+export { handler as GET, handler as POST }
