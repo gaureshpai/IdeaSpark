886f6e332bf71f6fdfeea70c0d7c4f05446b05ee
Gauresh G Pai <paigauresh@gmail.com>
2024-02-04 20:44:24 +0530

    Promptcard , route.jsx of prompt and the feed.jsx updated with the search for tags prompts display user with their email with the contribution for the particular prompts also addeded the function of copy the tag which is handlecopy and it did get executed well and fine

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,7 +5,7 @@ export const GET = async (request) => {
     try {
         await connectToDB()
 
-        const prompts = await Prompt.find({}).populate('creator')
+        const prompts = await Prompt.find({}).populate('creator');
 
         return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
