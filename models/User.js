492dfa9e1a5b5b0d20dc65cfe27d89ea2fe9c322
Gauresh G Pai <paigauresh@gmail.com>
2024-02-04 14:47:01 +0530

    user.js update

--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ import { Schema, model , models } from "mongoose";
 const UserSchema = new Schema({
     email:{
         type: String,
-        unique: [true, 'Email already exists'],
-        required: [true, 'Email is required'],
+        unique: [true, 'Email already exists!'],
+        required: [true, 'Email is required!'],
     },
     username: {
         type: String,
