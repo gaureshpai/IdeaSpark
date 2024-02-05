1f12f6eab2ed3a0b695e779b8a16cea72abd45ca
Gauresh G Pai <paigauresh@gmail.com>
2024-02-05 11:35:35 +0530

    databse.js remove attributes due to monngoose update

--- utils/database.js
+++ utils/database.js
@@ -13,8 +13,6 @@ export const connectToDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
             dbName: "share_prompt",
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
         })
 
         isConnected = true;
