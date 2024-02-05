c90578b9f992d922741716d2f4060bc6fc15240e
Gauresh G Pai <paigauresh@gmail.com>
2024-02-05 15:47:22 +0530

    cache nostore

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -27,7 +27,7 @@ const Feed = () => {
   const [searchedResults, setSearchedResults] = useState([]);
 
   const fetchPosts = async () => {
-    const response = await fetch("/api/prompt");
+    const response = await fetch("/api/prompt",{cache: "no-store"});
     const data = await response.json();
 
     setAllPosts(data);
