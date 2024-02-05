ff9819749310bc1755e6cb879869049e6b7c1888
Gauresh G Pai <paigauresh@gmail.com>
2024-02-05 16:34:11 +0530

    revalidatepath

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -7,7 +7,7 @@ export const GET = async (request) => {
         await connectToDB()
 
         const prompts = await Prompt.find({}).populate('creator')
-        // revalidatePath("/")
+        revalidatePath("/")
         return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 })
