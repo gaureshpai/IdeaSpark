f323d8e7013c9bf31b334d47154593f90ba7a7e2
Gauresh G Pai <paigauresh@gmail.com>
2024-02-05 15:57:08 +0530

    next: {revalidate: }}

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -27,7 +27,7 @@ const Feed = () => {
   const [searchedResults, setSearchedResults] = useState([]);
 
   const fetchPosts = async () => {
-    const response = await fetch("/api/prompt",{next: {revalidate: 0}});
+    const response = await fetch("/api/prompt",{next: {revalidate: 1}});
     const data = await response.json();
 
     setAllPosts(data);
