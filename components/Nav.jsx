2f551918985bb5500ed10264b2fee662150193a3
Gauresh G Pai <paigauresh@gmail.com>
2024-02-05 16:02:29 +0530

    null op

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -43,7 +43,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src={session?.user.image}
+                src={session?.user?.image}
                 width={37}
                 height={37}
                 className="rounded-full cursor-pointer"
@@ -75,7 +75,7 @@ const Nav = () => {
         {session?.user ? (
           <div className="flex">
             <Image
-              src={session?.user.image} // Corrected image path
+              src={session?.user?.image} // Corrected image path
               width={37}
               height={37}
               className="rounded-full cursor-pointer"
