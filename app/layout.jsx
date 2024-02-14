cd80a15b8b1166d6d50a8a5d46d1eb5337fc0544
Gauresh G Pai <paigauresh@gmail.com>
2024-02-14 16:28:18 +0530

    image to the title

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,19 @@ import Provider from "@components/Provider";
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
+  icons: {
+    icon:
+      [
+        'favicon.ico?v=4',
+      ],
+    apple: [
+      'apple-touch-icon.png?v=4',
+    ],
+    shortcut: [
+      'apple-touch-icon.png?v'
+    ]
+  },
+  manifest: '/site.webmanifest'
 }
 
 const RootLayout = ({ children }) => (
