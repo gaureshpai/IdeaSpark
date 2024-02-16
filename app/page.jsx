3f8f4c5ba19a0d350c97e955458846f9cd28ae88
Gauresh G Pai <paigauresh@gmail.com>
2024-02-16 23:00:55 +0530

    CHatGPT link added to the homepage suing href

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,6 @@
 import Feed from "@components/Feed";
+import ChatGPTLink from "@components/ChatGPTLink";
+
 const Home = () => (
   <section className='w-full flex-center flex-col'>
     <h1 className='head_text text-center'>
@@ -7,11 +9,16 @@ const Home = () => (
       <span className='orange_gradient text-center'> AI-Powered Prompts</span>
     </h1>
     <p className='desc text-center'>
-      Promptopia is an open-source AI prompting tool for modern world to
-      discover, create and share creative prompts
+      Promptopia is an open-source AI prompting tool for the modern world to
+      discover, create, and share creative prompts
+      Create your own prompts for others to use,
+      Copy the prompts you need to use and Use ChatGPT to get the answers
     </p>
-
-  <Feed />
+    <p className='desc text-center'>
+       {' '}
+      <ChatGPTLink />
+    </p>
+    <Feed />
   </section>
 );
 
