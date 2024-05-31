df58a9b19856f58fc4d1c6450af62356cda5927e
paigauresh <paigauresh@gmail.com>
2024-05-31 18:41:21 +0530

    re structuring initiated

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -22,7 +22,7 @@ const Nav = () => {
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         <Image
-          src="/assets/images/logo.svg"
+          src="/logo.png"
           alt="Promtopia logo"
           width={30}
           height={30}
