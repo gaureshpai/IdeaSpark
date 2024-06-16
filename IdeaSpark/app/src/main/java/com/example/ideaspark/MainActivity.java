e865e33ddc48c7fe98a8b510f414fd82be399531
paigauresh <paigauresh@gmail.com>
2024-06-16 13:32:07 +0530

    application draft 1 completed
    application download directory created where anyone can find the draft 1 application - application not completed redirect to website

--- IdeaSpark/app/src/main/java/com/example/ideaspark/MainActivity.java
+++ IdeaSpark/app/src/main/java/com/example/ideaspark/MainActivity.java
@@ -1,24 +1,27 @@
 package com.example.ideaspark;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btn=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url="ggideaspark.vercel.app/";
+                Uri u=Uri.parse("http://"+url);
+                Intent i=new Intent(Intent.ACTION_VIEW,u);
+                startActivity(i);
+            }
         });
     }
 }
