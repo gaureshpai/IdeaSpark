4733c75e369803eb3fc3c4acc46559bb85c1977b
paigauresh <paigauresh@gmail.com>
2024-09-08 19:58:20 +0530

    few button styling

--- components/ThemeSwitcher.jsx
+++ components/ThemeSwitcher.jsx
@@ -8,7 +8,12 @@ const ThemeSwitcher = () => {
     return (
         <button
             onClick={toggleTheme}
-            className="p-2 bg-gray-200 dark:bg-gray-800 text-black dark:text-white border rounded"
+            className=
+            {`p-3 rounded-lg border-2 transition-colors duration-300 
+                ${theme === 'light' ? 
+                'bg-gray-200 border-gray-300 hover:bg-gray-300 text-black' : 
+                'bg-gray-800 border-gray-600 hover:bg-gray-700 text-black'
+                }`}
         >
             Switch to {theme === 'light' ? 'Dark' : 'Light'} Theme
         </button>
